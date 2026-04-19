4e5a6d62b3329c757ba8a9e49897fdcb67b601ca
Gurkirat <gurkiratsg2008@gmail.com>
2026-04-19 13:06:17 +0000

    created new directory kotlin and added 1st program file

---
+++ kotlin/program1.kt
@@ -0,0 +1,12 @@
+fun main(){
+  var name = "Gurkirat"
+  var last_name = "Singh"
+
+  val a = 12
+  val b = 87
+  val c = a/b
+
+  println("The answer to " a + b + " is " + c)
+  println("My name is: " + name + " " + last_name)
+  println("hello world")
+}
